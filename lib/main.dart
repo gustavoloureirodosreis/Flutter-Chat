8f172f0872652c477bd05d3eb60988f1aa38390b
Gustavo <gusts92@gmail.com>
2020-05-12 20:31:16 -0300

    @setup

--- lib/main.dart
+++ lib/main.dart
@@ -33,7 +33,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Firebase Chat',
+      title: 'Nene Nene',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: Colors.blue,
