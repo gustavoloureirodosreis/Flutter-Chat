774af23aa4e38a101782be68d53c1ed26c5a5d64
Gustavo <gusts92@gmail.com>
2020-05-15 18:19:31 -0300

    @delayed push notifications with cloud tasks

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const { CloudTasksClient } = require('@google-cloud/tasks')
 admin.initializeApp();
 
 // // Create and Deploy Your First Cloud Functions
@@ -64,12 +65,80 @@ exports.addChatMessage = functions.firestore
             }
           }
         }
+      } 
+      // Notification delayed with Cloud Tasks
+      else {
+        let notificateInSeconds = messageData.timestamp.seconds;
+
+        const project = 'nene-nene-4';
+        const location = 'us-central1';
+        const queue = 'future-push';
+
+        const tasksClient = new CloudTasksClient();
+        const queuePath = tasksClient.queuePath(project, location, queue);
+
+        const url = `https://${location}-${project}.cloudfunctions.net/futurePushCallback`;
+        const payload = { chatData, messageData }
+
+        // Creates the task
+        const task = {
+          httpRequest: {
+            httpMethod: 'POST',
+            url,
+            body: Buffer.from(JSON.stringify(payload)).toString('base64'),
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          },
+          scheduleTime: {
+            seconds: notificateInSeconds
+          }
+        }
+
+        // Enqueue it
+        await tasksClient.createTask({ parent: queuePath, task })
       }
     }
-  });
+  }
+);
 
-// TO-DO: Usar uma functions.pubsub.schedule().onRun() (ver link: https://firebase.google.com/docs/functions/schedule-functions) a cada Xmin
-// Ou, fazer assim: https://medium.com/firebase-developers/how-to-schedule-a-cloud-function-to-run-in-the-future-in-order-to-build-a-firestore-document-ttl-754f9bf3214a
+exports.futurePushCallback =
+functions.https.onRequest(async (req, res) => {
+    const payload = req.body
+    try {
+      // Sends delayed notification
+        const chatData = payload.chatData
+        const messageData = payload.messageData
+        const memberInfo = chatData.memberInfo;
+        const senderId = messageData.senderId;
+        body += 'A delayed message from ' + memberInfo[senderId].name + ' arrived to you!';
+  
+        const payload = {
+          notification: {
+            title: chatData['name'],
+            body: body
+          }
+        };
+        const options = {
+          priority: 'high',
+          timeToLive: 60 * 60 * 24
+        };
+  
+        for (const userId in memberInfo) {
+          if (userId !== senderId) {
+            const token = memberInfo[userId].token;
+            if (token !== '') {
+              admin.messaging().sendToDevice(token, payload, options);
+            }
+          }
+        }
+        res.send(200)
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).send(error)
+    }
+})
 
 exports.onUpdateUser = functions.firestore
   .document('/users/{userId}')
